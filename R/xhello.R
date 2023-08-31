3369376805ee48efc6acb6a366ad1f1dabff52b2
Rainer Koellee <rainer.koelle@eurocontrol.int>
2023-08-31 09:22:30 +0200

    update of dummy function, check and cleaning of warnings

--- R/xhello.R
+++ R/xhello.R
@@ -1,11 +1,12 @@
-#' Title
+#' A friendly greeting for a person we know by name
 #'
-#' @param .name
+#' @param .name the name of the person we want to greet
 #'
-#' @return
+#' @return string comprising "Greeting + name provided
 #' @export
 #'
 #' @examples
+#' xhello("Stranger")
 xhello <- function(.name){
   paste0("Greetings ", .name)
 }
